38405eca8c556fad0f1853c1fb4c9a8a54cfe350
ShreeGithubsonu <107929321+ShreeGithubsonu@users.noreply.github.com>
2022-09-23 18:54:13 +0530

    Create class1.py

---
+++ class1.py
@@ -0,0 +1,23 @@
+Class 1
+
+print("Nice To Me you Tecknook")
+Nice To Me you Tecknook
+
+#Create a variable
+a="Hello Everyone"  #String
+print(a)
+print(type(a))
+Hello Everyone
+<class 'str'>
+
+#Creating variable in another way 
+_add_=4 #Integer 
+_add1_=3
+sum=_add_+_add1_
+print(sum)
+7
+
+n1=8.7   #float
+n2=4
+print(n1-n2)
+4.699999999999999
